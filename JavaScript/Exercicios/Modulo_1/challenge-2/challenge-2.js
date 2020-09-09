e2a3f1a7f790ac49104572d4ad25d95acc6f8b90
Ewerton Filipe <e.nascimento.pereira@avanade.com>
2020-09-09 16:18:21 -0300

    Challenge two completed

--- JavaScript/Exercicios/Modulo_1/challenge-2/challenge-2.js
+++ JavaScript/Exercicios/Modulo_1/challenge-2/challenge-2.js
@@ -1,35 +1,48 @@
 (function(win, doc) {
     'use strict';
 
+    const $do = doc.querySelector('[data-js="do"]');
     let v_number_one = 0;
     let v_number_two = 0;
 
-    const $do = doc.querySelector('[data-js="do"]');
-
     $do.addEventListener('click', captureValue, false);
-
-    function pairs(v_number_one, v_number_two) {
-      
-      const base = v_number_one - v_number_two
-      let increment = v_number_one;
-      let arr_number_pairs = [];
-      console.log(v_number_one);
-      // if(increment <= v_number_two) {
-      //   for (let increment; increment <= base; increment++) {
-      //     increment % 2 === 0 ? arr_number_pairs.push(icrement) : false;
-      //   }
-      // }
-
-      // return arr_number_pairs;
-
-    }
 
     function captureValue(e) {
       e.preventDefault();
       v_number_one = doc.querySelector('[data-js="number-one"]').value;
       v_number_two = doc.querySelector('[data-js="number-two"]').value;
+
+      return pairs(v_number_one, v_number_two);
     }
 
-    console.log(v_number_one, v_number_two);
+    function pairs(v_number_one, v_number_two) {
+      const number_one = Number(v_number_one);
+      const number_two = Number(v_number_two);
+
+      let limit = 0
+      
+      if(number_one < number_two)
+        limit =  number_two - number_one;
+
+      if(number_one > number_two)
+      return alert('Number one cannot be greater than number two');
+
+      let increment = number_one;
+      let arr_number_pairs = [];
+      if(increment <= number_two) {
+        for (increment; increment <= limit; increment++) {
+          if (increment % 2 === 0)
+            arr_number_pairs.push(increment);
+        }
+      }
+
+      const fragment = doc.createDocumentFragment();
+      const tagP = doc.createElement('p');
+      const textElement = doc.createTextNode('Numbers: ' + arr_number_pairs);
+      tagP.appendChild(textElement);
+      fragment.appendChild(tagP);
+
+      return doc.body.appendChild(fragment);
+    }
 
 })(window, document);
