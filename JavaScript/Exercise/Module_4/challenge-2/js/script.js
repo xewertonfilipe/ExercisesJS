19ba36efc5fe0fbb951c2000907154f2a0e1b8a5
Ewerton Filipe <e.nascimento.pereira@avanade.com>
2020-10-15 18:23:33 -0300

    new features

--- JavaScript/Exercise/Module_4/challenge-2/js/script.js
+++ JavaScript/Exercise/Module_4/challenge-2/js/script.js
@@ -3,110 +3,156 @@
 
   let fragment = doc.createDocumentFragment();
   axios.defaults.baseURL = 'https://api.github.com/users/';
-  const $inputNameUser = doc.querySelector('[data-js="user"]');
+  const $userName = doc.querySelector('[data-js="userName"]');
   const $btnSearchUser = doc.querySelector('[data-js="search-user"]');
   const $load = doc.querySelector('[data-js="load"]');
-  let $subtitle = doc.querySelector('[data-js="notification"]');
+  let $notification = doc.querySelector('[data-js="notification"]');
   let $repos = doc.querySelector('[data-js="repos"]');
 
 
   $btnSearchUser.addEventListener('click', startAplication, false);
+  $userName.addEventListener('keyup', ({key}) => {
+    if (key === 'Enter') startAplication();
+  });
     
   function startAplication() {
-    Toggleload();
-    removeOl();
+    toggleLoad();
+    removeListOfRepos();
     disableButton($btnSearchUser, 'disabled');
-    removeClassInSubTitle();
+    removeNotification();
     setTimeout(() => {
-    searchUser($inputNameUser.value);
+    searchUser($userName.value);
     }, 2000);
+  }
+
+  function toggleLoad() {
+    $load.classList.toggle('load-off');
+  }
+
+  function removeListOfRepos() {
+    const $div = doc.querySelector('[data-js="repos"]');
+    const $ol = doc.querySelector('[data-js="list-repos"]');
+    if($ol)
+      $div.removeChild($ol);
+  }
+
+  function disableButton(element, status) {
+    createAttribute(element, status, '');
+  }
+
+  function createAttribute(element, status, value) {
+    element.setAttribute(status, value);
+  }
+
+  // function removeNotification() {
+  //   if($notification.classList.contains('showNotification'))
+  //    return $notification.classList.remove('showNotification');
+  // }
+
+  function removeNotification() {
+    $notification.removeChild($notification.firstElementChild);
   }
 
   function searchUser(user) {
       axios.get(user + '/repos')
       .then(function(response) {
-        createRepos(response);
+        hasRepo(response);
       })
       .catch(function(error) {
         userNotFound();
       });
   }
 
-  function clearInput(input) {
-    return input.value = '';
+  function hasRepo(response) {
+    if(!response.data.length) {
+      toggleLoad();
+      addNotification('not has repos.');
+      enableButton($btnSearchUser);
+      clearInput($userName);
+      addFocus($userName);
+      return;
+    }
+    return createRepos(response);
+  }
+  
+  function addNotification(textNode) {
+    const tagFull = createTagWithTextNode('p', 'User "' + $userName.value + '" ' + textNode);
+    addFragmentInApp(tagFull, $notification);
+    addClassInNotification();
   }
 
-  function Toggleload() {
-    $load.classList.toggle('load-off');
+  
+
+  function createTagWithTextNode(tag, textNode) {
+    const element = doc.createElement(tag);
+    const text = doc.createTextNode(textNode);
+    element.appendChild(text);
+    return element;
+  }
+  
+  function addFragmentInApp(element, appLocation) {
+    fragment.appendChild(element);
+    return appLocation.appendChild(fragment);
   }
 
-  function disableButton(btn, status) {
-    createAttribute(btn, status, '');
+  function addClassInNotification() {
+    if(!$notification.classList.contains('showNotification'))
+       $notification.classList.add('showNotification');
   }
 
   function enableButton(btn) {
     btn.removeAttribute('disabled');
   }
 
-  function showSubTitleWithUser() {
-      $subtitle.classList.add('showNotification');
-      $subtitle.removeChild($subtitle.firstElementChild);
-      addElementChild($subtitle, 'List of repositories:');
-  }
-
-  function addElementChild(element, textNode) {
-    const newSubTitle = createTagWithTextNode('p', textNode + ' ' + $inputNameUser.value);
-    return element.appendChild(newSubTitle);
-  }
-
-  function removeClassInSubTitle() {
-    if($subtitle.classList.contains('showNotification'))
-     return $subtitle.classList.remove('showNotification');
-  }
-
-  function createRepos(response) {
-    Toggleload();
-    hasRepo(response);
-    showSubTitleWithUser($inputNameUser);
-    let ol = createTag('ol');
-    createAttribute(ol, 'data-js', 'list-repos');
-    addRepoInLiAndOl(response, ol);
-    addFragmentInApp(ol, $repos);
-    enableButton($btnSearchUser);
-    clearInput($inputNameUser);
-  }
-
-  function userNotFound() {
-    Toggleload();
-    addNotification('not found.');
-    enableButton($btnSearchUser);
-    clearInput($inputNameUser);
-    addFocus($inputNameUser);
+  function clearInput(input) {
+    return input.value = '';
   }
 
   function addFocus(element) {
     element.focus();
   }
 
-  function addClassInNotification() {
-    if(!$subtitle.classList.contains('showNotification'))
-       $subtitle.classList.add('showNotification');
+  function createRepos(response) {
+    toggleLoad();
+    showNotificationWithUser();
+    let olOfRepos = createTag('ol');
+    createAttribute(olOfRepos, 'data-js', 'list-repos');
+    addRepoInLiAndOl(response, olOfRepos);
+    addFragmentInApp(olOfRepos, $repos);
+    enableButton($btnSearchUser);
+    clearInput($userName);
+    addFocus($userName);
   }
 
-  function addNotification(text) {
-    $subtitle.removeChild($subtitle.firstElementChild);
-    const notUser = createTagWithTextNode('p', 'User "' + $inputNameUser.value + '" ' + text);
-    addFragmentInApp(notUser, $subtitle);
-    addClassInNotification();
+  function showNotificationWithUser() {
+      $notification.classList.add('showNotification');
+      $notification.removeChild($notification.firstElementChild);
+      addNotification('List of repositories: ');
+  }
+
+  // function addNotification(text) {
+  //   $notification.removeChild($notification.firstElementChild);
+  //   const text = createTagWithTextNode('p', 'User "' + $userName.value + '" ' + text);
+  //   addFragmentInApp(text, $notification);
+  //   addClassInNotification();
+  // }
+
+  // function addElementChild(element, textNode) {
+  //   const newNotification = createTagWithTextNode('p', textNode + $userName.value);
+  //   addFragmentInApp(newNotification, $notification);
+  // }
+
+  function userNotFound() {
+    toggleLoad();
+    addNotification('not found.');
+    enableButton($btnSearchUser);
+    clearInput($userName);
+    addFocus($userName);
   }
 
   function createTag(tag) {
     const element = doc.createElement(tag);
     return element;
-  }
-
-  function createAttribute(element, attribute, value) {
-    element.setAttribute(attribute, value);
   }
 
   function addRepoInLiAndOl(response, ol) {
@@ -116,35 +162,8 @@
     }
   }
 
-  function createTagWithTextNode(tag, textNode) {
-    const element = doc.createElement(tag);
-    const text = doc.createTextNode(textNode);
-    element.appendChild(text);
-    return element;
-  }
-
   function addValueInOl(ol, tagWithTextNode) {
     return ol.appendChild(tagWithTextNode);
-  }
-
-  function removeOl() {
-    const $div = doc.querySelector('[data-js="repos"]');
-    const $ol = doc.querySelector('[data-js="list-repos"]');
-    if($ol)
-      $div.removeChild($ol);
-  }
-
-  function addFragmentInApp(element, appLocation) {
-    fragment.appendChild(element);
-    return appLocation.appendChild(fragment);
-  }
-
-  function hasRepo(response) {
-    if(!response.data.length) {
-      console.log(response);
-      addNotification('not has repos.');
-      return;
-    }
   }
 
 })(window, document);
